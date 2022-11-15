ad66c53de9262fa768a89b262555b49d4d924682
MJ Brechner <mjbrechner@gmail.com>
2022-11-14 19:25:58 -0500

    added or-in-quotes and clear buttons & reworked some styling

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Or Adder
 by M.J. Brechner
 
-This is a basic tool to convert a list of items into a boolean string joining each item with an OR operator and encasing the entire output within parentheses.
+This is a basic tool to convert a list of items into a boolean string joining each item with an OR operator and encasing the entire output within parenthesis.
 
 Viewable at https://mjbrechner.github.io/or-adder/
 
--- javascript/index.js
+++ javascript/index.js
@@ -27,7 +27,7 @@ document.getElementById("text-output").innerText = "";
         // Replace line breaks with " or "
         editedText = editedText.replace(/[\r\n]/g, " or ");
 
-        // Enclose everything in parentheses
+        // Enclose everything in parenthesis
         editedText = editedText.replace(/^/, "(");
         editedText = editedText.replace(/$/, ")");
 
@@ -44,6 +44,53 @@ document.getElementById("text-output").innerText = "";
     }
 
     document.getElementById("copied-notice").style.visibility = "visible";
-    document.getElementById("text-output").style.backgroundColor = "#a8717f";
     setTimeout(copiedReturnToNormal, 5000);
+}
+
+// Function to reform and copy text with quotes around each line
+function orAddingWithQuotes() {
+    let originalText = document.getElementById("text-input").value;
+
+// Clear any existing contet out of the text-output box
+document.getElementById("text-output").innerText = "";
+
+    // Only continue if there is actually something in the textbox
+    if (originalText !== "") {
+
+        // Delete ending spaces
+        let editedText = originalText.replace(/\s*$/, '');
+
+        // Delete lines that are completely blank
+        editedText = editedText.replace(/\n\s*\n/g, '\n');
+
+        // Replace line breaks with "or" in quotes
+        editedText = editedText.replace(/[\r\n]/g, "\" or \"");
+
+        // Make sure to add quotes to the beginning of the first item and the end of the last item
+        editedText = editedText.replace(/^/, "\"");
+        editedText = editedText.replace(/$/, "\"");
+
+        // Enclose everything in parenthesis
+        editedText = editedText.replace(/^/, "(");
+        editedText = editedText.replace(/$/, ")");
+
+        // Display results
+        document.getElementById("text-output").innerText = editedText;
+
+        // Copy output to clipboard and display message
+        textToCopy = document.getElementById("text-output").value;
+        navigator.clipboard.writeText(textToCopy);
+        document.getElementById("copied-notice").innerText = "Text reformatted and copied to clipboard.";
+    } else {
+        // No text to process
+        document.getElementById("copied-notice").innerText = "Please add the text to be processed in the box to the left.";
+    }
+
+    document.getElementById("copied-notice").style.visibility = "visible";
+    setTimeout(copiedReturnToNormal, 5000);
+}
+
+// Function to clear the output field
+function orClearing() {
+    document.getElementById("text-output").innerText = "";
 }
